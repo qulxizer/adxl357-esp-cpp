46fddd32c486d2cd1d2599a6ef1505b49014638f
mahdi <mahdi@1jz.cc>
2026-08-15 17:48:40 +0300

    add: nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "ESP dev shell";
+
+  inputs = {
+    nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
+    nixpkgs.follows = "nixpkgs-esp-dev/nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-esp-dev,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ];
+        config.permittedInsecurePackages = [
+          "python3.13-ecdsa-0.19.1"
+        ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "geary";
+        buildInputs = with pkgs; [
+          esp-idf-full
+        ];
+      };
+    };
+}
