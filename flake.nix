61698f8d66ef82754ca85013b9c25dbff8c608cf
mahdi <mahdi@1jz.cc>
2026-08-15 23:50:37 +0300

    add: flake and configure clangd

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
-  description = "ESP dev shell";
-
   inputs = {
-    nixpkgs-esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
-    nixpkgs.follows = "nixpkgs-esp-dev/nixpkgs";
+    nixpkgs-esp-dev = {
+      url = "github:mirrexagon/nixpkgs-esp-dev";
+    };
   };
-
   outputs =
     {
       self,
@@ -18,16 +16,27 @@
         inherit system;
         overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ];
         config.permittedInsecurePackages = [
-          "python3.13-ecdsa-0.19.1"
+          "python3.14-ecdsa-0.19.2"
         ];
       };
+
+      # change esp-idf-esp32 to whatever is the toolchain you're using
+      idf = nixpkgs-esp-dev.packages.${system}.esp-idf-xtensa.override (final: {
+        toolsToInclude = final.toolsToInclude ++ [
+          "esp-clang"
+        ];
+      });
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        name = "adxl357";
         buildInputs = with pkgs; [
-          esp-idf-full
+          idf
         ];
+
+        # passed as --query-driver=$CLANGD_QUERY_DRIVER to clangd
+        shellHook = ''
+          export CLANGD_QUERY_DRIVER=`which xtensa-esp32-elf-g++`
+        '';
       };
     };
 }
